be68348bdc7b8ab0955c6d292784634413f26d2e
Marisa Ervin <mtervin@ucsc.edu>
2023-05-02 19:49:23 -0700

    god y

--- Rocket Patrol/src/scenes/play.js
+++ Rocket Patrol/src/scenes/play.js
@@ -13,6 +13,8 @@ class Play extends Phaser.Scene {
     create() {
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starnebula').setOrigin(0, 0);
 
+        this.sound.play('sfx_music');
+
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
