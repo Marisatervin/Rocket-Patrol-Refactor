0fe815c478dd704ed7250b7fa53ad770fc7bff48
Marisa Ervin <mtervin@ucsc.edu>
2023-05-02 21:27:09 -0700

    crying

--- Rocket Patrol/src/scenes/play.js
+++ Rocket Patrol/src/scenes/play.js
@@ -5,7 +5,7 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.image('rocket', './assets/Rocket.png');
-        this.load.image('spaceship', './assets/Spaceship.png');
+        this.load.atlas('spaceship', './assets/Spaceship.png', 'assets/sprites.json');
         this.load.image('stealthship', './assets/stealthship.png');
         this.load.image('starnebula', './assets/starnebula.png');
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
@@ -26,6 +26,11 @@ class Play extends Phaser.Scene {
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
         
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship01.animations.add(
+            'flying',
+            Phaser.Animation.generateFrameNumbers('flying', 1, 3),
+            true
+        ); 
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
 
